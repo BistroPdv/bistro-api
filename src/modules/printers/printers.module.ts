0461b9a55cd3d279e7779492d12b15822858f39c
Diego Carlos <53383018+diegcarlos@users.noreply.github.com>
2025-10-03 16:03:49 -0400

    Integra o WebsocketModule no PrintersModule, permitindo a comunicação em tempo real e suporte a eventos de atualização de impressoras.

--- src/modules/printers/printers.module.ts
+++ src/modules/printers/printers.module.ts
@@ -1,10 +1,11 @@
+import { WebsocketModule } from '@/websocket/websocket.module';
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../..//database/prisma/prisma.module';
 import { PrintersController } from './printers.controller';
 import { PrintersService } from './printers.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, WebsocketModule],
   controllers: [PrintersController],
   exports: [PrintersService],
   providers: [PrintersService],
